20c8ca9cbd0ba4a2a9fe9ebd28ee5c52335bb31e
Wilhelm Michaelsen <wm@wilhelmmichaelsen.com>
2018-03-06 19:45:45 +0100

    Re arranged files

---
+++ Wille/pussi.py
@@ -0,0 +1,16 @@
+import requests
+
+API_KEY = "EBHYLXXG3AQGI8XN"
+
+r = requests.get('https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=GOOG&outputsize=full&apikey='+API_KEY+'&dataType=CSV')
+
+if r.status_code != requests.codes.ok:
+    print "Bad request"
+
+myJSON = r.json()
+stockData = myJSON["Time Series (Daily)"]
+
+interval = 7
+candleStickInterval = 1
+
+for key in stockData
