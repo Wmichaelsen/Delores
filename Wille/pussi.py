baa0681269fefb54e0f0016bca8476f1c8f3b4a5
Hugo Bläckberg <hugoblackberg@b.local>
2018-03-07 13:44:49 +0100

    Added calculation for wedges

--- Wille/pussi.py
+++ Wille/pussi.py
@@ -1,6 +1,5 @@
 import requests
 
-# Wille is a little pussy
 API_KEY = "EBHYLXXG3AQGI8XN"
 
 r = requests.get('https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=GOOG&outputsize=full&apikey='+API_KEY+'&dataType=CSV')
@@ -15,3 +14,20 @@ interval = 7
 candleStickInterval = 1
 
 for key in stockData
+
+
+
+#---- Wedge calculator -----
+# Undre har storre k-varde an ovre
+# above_slope is the five highest values
+# below_slope is the five lowest values
+
+# if above_slope > 0 and below_slope > 0 and above_slope < below_slope:  # Rising wedge
+#     print "Rising wedge"
+#
+# elif above_slope < 0 and below_slope < 0 and above_slope < below_slope:  # Falling wedge
+#     print "Falling wedge"
+#
+# else:
+#     print "No wedge found"
+
