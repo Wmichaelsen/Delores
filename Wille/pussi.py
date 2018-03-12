4437b76da9c2efc4263ea8b04161693c9b080d60
Wilhelm Michaelsen <wm@wilhelmmichaelsen.com>
2018-03-12 10:15:23 +0100

    started working on finding next epok high/low

--- Wille/pussi.py
+++ Wille/pussi.py
@@ -23,10 +23,9 @@ def getSubList(list, n):
     return newList
 
 # Calculate wedges. Returns a list containing two list, each holding wedge data
-def getWedges(closePrices):
+def getWedges(sortedClosePrices):
 
     # Sort close prices and split into highest and lowest
-    sortedClosePrices = sorted(closePrices, key=itemgetter(1))
     lows = sortedClosePrices[0:(interval/2)]
     highs = sortedClosePrices[interval/2:interval]
 
@@ -40,7 +39,7 @@ def getWedges(closePrices):
     slope_high, intercept_high, r_value_high, p_value_high, std_err_high = stats.linregress(x_high, y_high)
     slope_low, intercept_low, r_value_low, p_value_low, std_err_low = stats.linregress(x_low, y_low)
 
-    plotWedges(x_low, y_low, intercept_low, slope_low, x_high, y_high, intercept_high, slope_high)
+    #plotWedges(x_low, y_low, intercept_low, slope_low, x_high, y_high, intercept_high, slope_high)
 
     return [[slope_high, intercept_high, r_value_high, p_value_high, std_err_high],[slope_low, intercept_low, r_value_low, p_value_low, std_err_low]]
 
@@ -52,6 +51,25 @@ def plotWedges(x_low, y_low, intercept_low, slope_low, x_high, y_high, intercept
     plt.plot(x_low, intercept_low + slope_low*x_low, 'b-', label='low price line')
     plt.legend()
     plt.show()
+
+# Determines bull/bear for coming epok
+def isBull(nextEpok, currentHigh):
+    nextEpokPrices = []
+    for date in nextEpok:
+        currentClosePrice = nextEpok[date]["4. close"]
+        nextEpokPrices.append(currentClosePrice)
+
+    sortedNextEpokPrices = sorted(nextEpokPrices)
+    nextEpokHighest = sortedNextEpokPrices[len(sortedNextEpokPrices)-1]
+    nextEpokLowest = sortedNextEpokPrices[0]
+
+    if nextEpokHighest < currentHigh:
+        return False
+    else:
+        if abs(nextEpokHighest-currentHigh) > abs(currentHigh-nextEpokLowest):
+            return True
+        else:
+            print False
 
 #---- DATA GATHERING ----
 r = requests.get('https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=GOOG&outputsize=full&apikey='+API_KEY)
@@ -86,7 +104,9 @@ for dateKey in sortedStockDataKeys:
 #---- WEDGE CONSTRUCTION ----
 
 # Iterate over each epok containing 5 dictionaries
-for epok in epoks[0:1]:
+wedges = []
+counter = 0
+for epok in epoks[0:2]:
 
     # List of all close prices in current epok
     closePrices = []
@@ -99,8 +119,16 @@ for epok in epoks[0:1]:
         unixTime = datetime.datetime.strptime(date, '%Y-%m-%d').strftime("%s")
         closePrices.append([unixTime, currentClosePrice])
 
-    wedges = getWedges(closePrices)
+    sortedClosePrices = sorted(closePrices, key=itemgetter(1))
+    currentEpokHighest = sortedClosePrices[len(sortedClosePrices)-1][1]
+    wedges.append(getWedges(sortedClosePrices))
 
+    # Check if current wedge leads to higher or lower price next epok
+    nextEpok = epoks[counter+1]
+
+    print isBull(nextEpok, currentEpokHighest)
+
+    counter += 1
 
 #---- Wedge calculator -----
 # Undre har storre k-varde an ovre
